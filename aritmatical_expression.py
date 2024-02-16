8e928cb7e39203094aa28b34748e36e874cff30c
onoda <onodatorncity@gmail.com>
2024-02-16 13:36:08 +0700

    initial commit

---
+++ aritmatical_expression.py
@@ -0,0 +1,23 @@
+"""
+fName = "Your"
+lName = "Name"
+name = f"{fName} {lName}"
+
+a = 5
+b = 3
+
+print("Name = ",name)
+print("a + b = ",a+b)
+print("a - b = ",a-b)
+print("a * b = ",a*b)
+print("a / b = ",a/b)
+"""
+
+a = int(input("Masukkan nilai a = "))
+b = int(input("Masukkan nilai b = "))
+
+print("\nHasil\n")
+print("a + b = ",a+b)
+print("a - b = ",a-b)
+print("a * b = ",a*b)
+print("a / b = ",a/b)
